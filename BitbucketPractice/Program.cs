35990d2042e48318abef5510bda981ca4a98819e
engineer_dude_3 <paulphillips@phillipsengineer.com>
2021-03-17 20:52:03 -0700

    Initial commit

--- BitbucketPractice/Program.cs
+++ BitbucketPractice/Program.cs
@@ -6,7 +6,7 @@ namespace BitbucketPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello all!");
         }
     }
 }
